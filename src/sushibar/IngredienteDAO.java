b91e8a114e9820217db303062cf5671df3ad1004
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-05-10 21:57:51 -0300

    Update IngredienteDAO.java

--- src/sushibar/IngredienteDAO.java
+++ src/sushibar/IngredienteDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sushibar;
 
 /**
@@ -10,5 +5,18 @@ package sushibar;
  * @author Giovane
  */
 public class IngredienteDAO {
+    
+    public void inserir(Ingrediente ingrediente) {
+		String sql = "INSERT INTO ingredientes (nome, quantidade) VALUES (?, ?)";
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, ingrediente.getNome());
+			stmt.setInt(2, ingrediente.getQuant());
+			stmt.execute();
+			stmt.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Erro: " + e);
+		}
+	}
     
 }
