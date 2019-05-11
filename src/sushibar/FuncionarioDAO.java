6df48a828a0b912eb7cf1a2608d80f0359a310c8
Giovane Alvares <50254355+GiovaneA@users.noreply.github.com>
2019-05-10 22:08:20 -0300

    FuncDAO BETA 1

--- src/sushibar/FuncionarioDAO.java
+++ src/sushibar/FuncionarioDAO.java
@@ -1,7 +1,38 @@
 
 package sushibar;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class FuncionarioDAO {
+    
+    private Connection conexao;
+    private PreparedStatement stmt;
+    
+    public FuncionarioDAO() {
+        this.conexao = new Conexao().getConexao();
+    }
+    
+    public void inserir(Funcionario funcionario) {
+        String sql = "INSERT INTO funcionario (nome, data_nasc, endereco, salario, cargo, cpf) VALUES (?,?,?,?,?,?)";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, funcionario.getNome());
+            stmt.setDate(2, funcionario.getNasc());
+            stmt.setString(3, funcionario.getEnd());
+            stmt.setDouble(4, funcionario.getSalario());
+            stmt.setString(5, funcionario.getCargo());
+            stmt.setInt(6, funcionario.getCPF());
+            stmt.execute();
+            stmt.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    
+    
     
 }
