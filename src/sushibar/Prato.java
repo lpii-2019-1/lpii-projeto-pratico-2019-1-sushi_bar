b11d6a5e6686078b9408bbd7e925edb058ae1317
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-06-07 20:27:18 -0300

    Update Prato.java

--- src/sushibar/Prato.java
+++ src/sushibar/Prato.java
@@ -7,6 +7,36 @@ public class Prato {
     String nome;
     ArrayList<Ingrediente> nomeIng;
     double preco;
+
+    public Prato(String nome, ArrayList<Ingrediente> nomeIng, double preco) {
+        this.nome = nome;
+        this.nomeIng = nomeIng;
+        this.preco = preco;
+    }
+
+    public ArrayList<Ingrediente> getNomeIng() {
+        return nomeIng;
+    }
+
+    public void setNomeIng(ArrayList<Ingrediente> nomeIng) {
+        this.nomeIng = nomeIng;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
     
     
     public void addIng(String ing){
