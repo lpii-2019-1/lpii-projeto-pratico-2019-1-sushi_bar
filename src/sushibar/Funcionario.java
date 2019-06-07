02a2ccb20242c0c2573110f87cdf33ba88603078
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-06-07 20:23:17 -0300

    Update Funcionario.java

--- src/sushibar/Funcionario.java
+++ src/sushibar/Funcionario.java
@@ -1,4 +1,3 @@
-
 package sushibar;
 import java.util.Calendar;
 import java.text.DateFormat;
@@ -13,13 +12,64 @@ public class Funcionario {
     double salario;
     String cargo;
     int cpf;
-    
-    
-    public String registrarFuncionario(){
-    
-     return nome;   
-    
+
+    public Funcionario(String nome, Calendar data_nasc, String endereco, int cpf) {
+        this.nome = nome;
+        this.data_nasc = data_nasc;
+        this.endereco = endereco;
+        this.cpf = cpf;
     }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Calendar getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(Calendar data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+
     
     
 }
