02b82e598d9f22a900617af005c065595e3d8a3d
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-06-07 20:25:06 -0300

    Update Pedido.java

--- src/sushibar/Pedido.java
+++ src/sushibar/Pedido.java
@@ -3,10 +3,36 @@ import java.util.ArrayList;
 
 public class Pedido {
     
-     ArrayList<Prato> pratos;
+    ArrayList<Prato> pratos;
     double pracoTotal;
     String obs;
     Funcionario funcionario;
+
+    public ArrayList<Prato> getPratos() {
+        return pratos;
+    }
+
+    public void setPratos(ArrayList<Prato> pratos) {
+        this.pratos = pratos;
+    }
+
+    public double getPracoTotal() {
+        return pracoTotal;
+    }
+
+    public void setPracoTotal(double pracoTotal) {
+        this.pracoTotal = pracoTotal;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+    
+    
     
     public void pedir(){
     
