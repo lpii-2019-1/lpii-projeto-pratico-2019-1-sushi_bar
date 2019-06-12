217f17ef7be9fc981edba63374d0fa8cfa6fb86d
Giovane Alvares <50254355+GiovaneA@users.noreply.github.com>
2019-06-12 19:57:14 -0300

    Update v1

--- src/sushibar/Funcionario.java
+++ src/sushibar/Funcionario.java
@@ -1,25 +1,20 @@
+
 package sushibar;
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.util.Date;
+
 
 
 public class Funcionario {
     
+    int idFuncionario;
     String nome;
-    Calendar data_nasc;
-    String endereco;
-    double salario;
-    String cargo;
-    int cpf;
 
-    public Funcionario(String nome, Calendar data_nasc, String endereco, int cpf) {
-        this.nome = nome;
-        this.data_nasc = data_nasc;
-        this.endereco = endereco;
-        this.cpf = cpf;
+    public int getIdFuncionario() {
+        return idFuncionario;
     }
-    
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
 
     public String getNome() {
         return nome;
@@ -28,48 +23,8 @@ public class Funcionario {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public Calendar getData_nasc() {
-        return data_nasc;
-    }
-
-    public void setData_nasc(Calendar data_nasc) {
-        this.data_nasc = data_nasc;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public int getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-    
-
+   
     
     
+   
 }
