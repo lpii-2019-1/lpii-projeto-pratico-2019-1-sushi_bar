fac4f08001d1304688f02d881e1a527b39539047
Giovane Alvares <50254355+GiovaneA@users.noreply.github.com>
2019-06-12 20:02:05 -0300

    Update v1

--- src/sushibar/PratoDAO.java
+++ src/sushibar/PratoDAO.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sushibar;
 
-/**
- *
- * @author Giovane
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class PratoDAO {
+    private Connection conexao;
+    private PreparedStatement stmt; 
+    
+    public PratoDAO(){
+    
+        this.conexao = new Conexao().getConnection();
+    
+    }
+    
+    public Prato pesquisaPrato(int id) {
+        String sql = "SELECT * FROM Prato WHERE idPrato = ?";
+        try {
+            
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            Prato prato = new Prato();
+            
+            if (rs.next()) {
+                
+                prato.setIdPrato(rs.getInt("idPrato"));
+                prato.setNome(rs.getString("nome"));
+                prato.setPreco(rs.getDouble("preco"));
+        
+            }
+            stmt.close();
+            return prato;
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
     
 }
